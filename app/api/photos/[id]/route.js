12ae98e84e8afb1a24a0aad1ad711ea33d5cfe7a
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-04-07 19:29:43 +0600

    OK

--- app/api/photos/[id]/route.js
+++ app/api/photos/[id]/route.js
@@ -4,7 +4,6 @@ import { NextResponse } from "next/server";
 
 export async function GET(request, { params }) {
   const photoId = params?.id;
-
   const data = await getPhotoById(photoId);
 
   return NextResponse.json(data);
